bf34e2b561465862afc5dddfd343ed3e5cd24e2e
ValeriaKul <lera.dribnyuk@mail.ru>
2023-03-31 13:55:10 +0300

    31 Mar 2023

---
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "./breadcrumbs.module.css";
+
+const Breadcrumbs = (props) => {
+  const { pathname } = useLocation();
+  return (
+    <div className={styles.wrapper}>
+      <img src="/images/breadcrumb-logo.png" alt="" className={styles.icon} />
+      <h1>{props.title}</h1>
+
+      <div className={styles.breadcrumb}>
+        <Link to="/"> Home</Link>
+        &#8594;
+        <Link to={pathname}>{props.title}</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Breadcrumbs;
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './product.module.css'
+
+const Product = (props) => {
+   
+    return (
+        <div className={props.gridView ? styles.card : styles["card-list"]}>
+            <img src={props.img} alt="" className={styles.img} />
+            <div className={styles.info}>
+                <Link to={`/catalog/${props.id}`} className={styles.name}>
+                {props.title}
+                </Link>
+                <h4 className={styles.price}>
+                    {props.price}
+                </h4>
+                <p>{new Date(props.date).toString()}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
